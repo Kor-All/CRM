32cfc6f2d886fa4a23b90013162f459af816c18d
ak <koral1984ex@gmail.com>
2021-03-15 04:19:10 +0200

    ALLOWED_HOSTS

--- crm/settings.py
+++ crm/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '3uu-+kbk)rsx)h9=2a=#@=v8@r@v9z
 # DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['myapp-crm.herokuapp.com','127.0.0.1']
 
 
 # Application definition
